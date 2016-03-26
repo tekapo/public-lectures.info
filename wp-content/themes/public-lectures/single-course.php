d67163a3c24b4972bd6738e95ad62608cf6ef140
JOTAKI, Taisuke <tekapo@gmail.com>
2016-03-26 13:05:01 +0900

    misc

--- wp-content/themes/public-lectures/single-course.php
+++ wp-content/themes/public-lectures/single-course.php
@@ -10,66 +10,126 @@ get_header();
 <div class="row">
     <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 font" >
 
-        <?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
 
-            <?php
-            get_template_part( 'content', 'single' );
+				</header><!-- .entry-header -->
 
-            $syllabus          = SCF::get( 'syllabus' );
-            $teacher           = SCF::get( 'teacher' );
-            $start_date        = SCF::get( 'start-date' );
-            $end_date          = SCF::get( 'end-date' );
-            $start_application = SCF::get( 'start-application' );
-            $end_application   = SCF::get( 'end-application' );
-            $class_day         = SCF::get( 'day' );
-            $class_time        = SCF::get( 'time' );
+				<div class="entry-content">
+					<?php
+					$url = '';
+					if ( has_post_thumbnail() ):
+						?>
+						<?php $url = wp_get_attachment_url( get_post_thumbnail_id() ); ?>
+						<div class="ent-img">
+							<a href="<?php the_permalink(); ?>"><img src="<?php echo $url; ?>" class="img-responsive img-thumbnail"></a>
+						</div>
+					<?php endif; ?>
+					<?php // the_content(); ?>
+					<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . __( 'Pages:', 'bricks' ),
+						'after'	 => '</div>',
+					) );
+					?>
+				</div><!-- .entry-content -->
+				<div id="post-nav"><center><?php bricks_posts_nav(); ?></center></div>
+			</article><!-- #post-## -->
+			<?php
+//			get_template_part( 'content', 'single' );
 
-            $school_id   = SCF::get( 'school-name' );
-            $page        = get_page( $school_id[0] );
-            $school_name = $page->post_title;
+			if ( class_exists( SCF ) ) {
 
-            $format = '
-            <table>
-            $syllabus
+				$course_title			 = get_the_title();
+				$category_list			 = get_the_category_list( ',' );
+				$tag_list				 = get_the_tag_list( '', ', ' );
+				$syllabus				 = SCF::get( 'syllabus' );
+				$syllabus_original_url	 = SCF::get( 'syllabus_original_url' );
+				$teacher				 = SCF::get( 'teacher' );
+				$start_date				 = SCF::get( 'start-date' );
+				$end_date				 = SCF::get( 'end-date' );
+				$start_application		 = SCF::get( 'start-application' );
+				$end_application		 = SCF::get( 'end-application' );
+				$class_day				 = SCF::get( 'day' );
+				$class_time				 = SCF::get( 'time' );
+
+				$school_id	 = SCF::get( 'school-name' );
+				$page		 = get_page( $school_id[0] );
+				$school_name = $page->post_title;
+				
+				var_dump($class_day);
+				
+				foreach ($class_day as $day) {
+					$days .= $day.'、';
+				}
+
+				echo <<< EOL
+				
+			<table class='course-table'>
+				<tr>
+					<th>コース名</th>
+					<td>$course_title</td>
+				</tr>
+				<tr>
+					<th>概要</th>
+					<td>
+						<blockquote cite="$syllabus_original_url">
+						$syllabus
+						</blockquote>
+						<div class="quote-from">
+							<a href="$syllabus_original_url">
+								$syllabus_original_url
+							</a>より引用
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th>カテゴリー</th>
+					<td>$category_list</td>
+				</tr>
+				<tr>
+					<th>タグ</th>
+					<td>$tag_list</td>
+				</tr>
                 <tr>
                     <th>講師</th>
-                    <td>%1$s</td>
+                    <td>$teacher</td>
                 </tr>
                 <tr>
                     <th>開講期間</th>
-                    <td>%2$s - %3$s</td>
+                    <td>$start_date - $end_date</td>
                 </tr>
                 <tr>
                     <th>開講曜日</th>
-                    <td>%4$s</td>
+                    <td>$days</td>
                 </tr>
                 <tr>
                     <th>開講時間</th>
-                    <td>%5$s</td>
+                    <td>$class_time</td>
                 </tr>
                 <tr>
                     <th>開催校</th>
-                    <td>%8$s</td>
+                    <td>$school_name</td>
                 </tr>
             </table>
-            ';
-            echo sprintf(
-                    $format, $teacher, $start_date, $end_date, $class_day[0], $class_time, $start_application, $end_application, $school_name
-            );
-            ?>
+EOL;
+			}
+			?>
 
-            <?php
-            // If comments are open or we have at least one comment, load up the comment template
-            if ( comments_open() || '0' != get_comments_number() ) :
-                comments_template();
-            endif;
-            ?>
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || '0' != get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
 
-        <?php endwhile; // end of the loop.   ?>
+		<?php endwhile; // end of the loop.   ?>
 
     </div><!-- #main -->
     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 font">
-        <?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
     </div>
 </div><!-- #primary -->
 
